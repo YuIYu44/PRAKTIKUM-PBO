4b68d64849ccbe5aff4bf2d1842dd15c2a7f57fc
YuIYu44 <ayu0lestari124@gmail.com>
2023-03-28 00:47:17 +0800

    postest4

---
+++ postest4_054/src/main/java/postest1_054/Pesanan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package postest1_054;
+
+/**
+ *
+ * @author HP
+ */
+public class Pesanan {
+    private String nama;
+    private int harga;
+    public Pesanan(String namas,int hargas){
+        this.nama=namas;
+        this.harga=hargas;}
+
+    public String getnama() {
+        return nama;}
+
+    public void setnama(String namas) {
+        this.nama = namas;}
+
+    public int getharga() {
+        return harga;}
+
+    public void setharga(int hargas) {
+        this.harga = hargas;}}
